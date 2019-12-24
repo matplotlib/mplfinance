6ef8fd6f480f4ab108c67026a77292112df412e9
Dino <Dino@MacDino.local>
2019-12-23 20:43:05 -0500

    test without sys.path.insert

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ import sys
 # third-party code may rely on sys.path documentation conformance:
 #    As initialized upon program startup, the first item of this list, path[0], is
 #    the directory containing the script that was used to invoke the Python interpreter.
-sys.path.insert(1,pkg_location)
+###sys.path.insert(1,pkg_location)
 # for safety, make sure this: is the only import after changing sys.path
-from mplfinance import __version__
+###from mplfinance import __version__
+__version__='0.11.0a0' # test hard-coded so avoid sys.path.insert (maybe that's breaking travis checks).
 
 with open('README.md') as f:
     long_description = f.read()
