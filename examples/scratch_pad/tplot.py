1ccae64f6e56c29d323dbd97089693b25d61d8c1
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2020-01-09 19:53:01 -0500

    cleanup examples folder

--- examples/scratch_pad/tplot.py
+++ examples/scratch_pad/tplot.py
@@ -1,5 +1,5 @@
 import pandas as pd
-daily = pd.read_csv('data/SP500_NOV2019_Hist.csv',index_col=0,parse_dates=True)
+daily = pd.read_csv('../data/SP500_NOV2019_Hist.csv',index_col=0,parse_dates=True)
 daily.head(3)
 
 #   idf = pd.read_csv('data/SP500_NOV2019_IDay.csv',index_col=0,parse_dates=True)
@@ -14,6 +14,6 @@ mpf.__file__
 mpf.plot(daily,mav=7)
 mpf.plot(daily,volume=True)
 
-df = pd.read_csv('data/yahoofinance-SPY-20080101-20180101.csv',index_col=0,parse_dates=True)
+df = pd.read_csv('../data/yahoofinance-SPY-20080101-20180101.csv',index_col=0,parse_dates=True)
 mpf.plot(df[700:850],type='bars',no_xgaps=True,mav=(20,40),figscale=0.7)
 mpf.plot(df[700:850],type='bars',volume=True,no_xgaps=True,mav=(20,40),figscale=0.7)
