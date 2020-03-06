b62c91f5100b2667619e6418cdcc6090c1395561
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2020-03-05 22:21:08 -0500

    spruce up tests a tad

--- examples/mpftests/test_addplot.py
+++ examples/mpftests/test_addplot.py
@@ -8,8 +8,8 @@ print('mpf.__version__ =',mpf.__version__)                 # for the record
 print("plt.rcParams['backend'] =",plt.rcParams['backend']) # for the record
 
 import subprocess
-result = subprocess.run(['pwd'], stdout=subprocess.PIPE)
-print('result.stdout=',result.stdout)
+pwd = subprocess.run(['pwd'], stdout=subprocess.PIPE)
+print('pwd.stdout=',pwd.stdout)
 
 
 df = pd.read_csv('../data/SPY_20110701_20120630_Bollinger.csv',index_col=0,parse_dates=True)
@@ -17,17 +17,27 @@ print('df.shape='  , df.shape  )
 print('df.head(3)=', df.head(3))
 print('df.tail(3)=', df.tail(3))
 
+prefix='addplot'
+tdir='test_images/'
+refd='reference_images/'
+os.system('rm -f '+tdir+prefix+'*.jpg')
+os.system('rm -f '+tdir+prefix+'*.png')
 
-mpf.plot(df,volume=True,savefig='test01.jpg')
+# ---- Test 01 -----
 
-os.system('ls -l test01.jpg')
+fname=prefix+'01.jpg'
+mpf.plot(df,volume=True,savefig=tdir+fname)
 
-rc = os.system('diff test01.jpg reference_images/test01.jpg')
+os.system('ls -l '+tdir+fname)
+rc = os.system('diff '+tdir+fname+' '+refd+fname)
 assert rc == 0
 
-apdict = mpf.make_addplot(df['LowerB'])
-mpf.plot(df,volume=True,addplot=apdict,savefig='test02.jpg')
+# ---- Test 02 -----
 
-os.system('ls -l test02.jpg')
-rc = os.system('diff test02.jpg reference_images/test02.jpg')
+fname=prefix+'02.jpg'
+apdict = mpf.make_addplot(df['LowerB'])
+mpf.plot(df,volume=True,addplot=apdict,savefig=tdir+fname)
+
+os.system('ls -l '+tdir+fname)
+rc = os.system('diff '+tdir+fname+' '+refd+fname)
 assert rc == 0
