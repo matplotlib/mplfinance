c89b4b9ae027dc72039bcc753a6bbe33b925c279
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2020-03-19 21:36:36 -0400

    use `os` methods to make work on windows or linux

--- tests/test_original_flavor.py
+++ tests/test_original_flavor.py
@@ -1,14 +1,20 @@
 import glob
 import os
+import os.path
 import subprocess
 
 def test_original_flavor_e2e_examples():
     """Run e2e tests for  folder"""
 
-    os.chdir("examples/original_flavor")
-    exs = glob.glob('*.py')
+    # os.path.join makes this work on windows as well as linux
+    new_dir  = os.path.join('examples','original_flavor')
+    orig_dir = os.path.join('..','..')
+
+    os.chdir(new_dir)
+
+    exs = glob.glob('*.py') 
     try:
         for ex in exs:
             subprocess.check_call(["python", ex])
     finally:
-        os.chdir("../../")
+        os.chdir(orig_dir)
