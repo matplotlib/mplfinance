4bd0dd4dac3eb883aeca16a710894220b3f04c8c
Aaron Soellinger <free-soellingeraj@Lydias-MacBook-Pro.local>
2020-05-04 16:14:10 +0200

    changed names of variables to avoid in keyword

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -3,10 +3,13 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check versions.')
-    parser.add_argument('--pr', type=str, help='mplfinance.__version__ for the PR')
-    parser.add_argument('--in', type=str, help='mplfinance.__version__ for the `master` branch')
+    parser.add_argument('--pr_v', type=str, help='mplfinance.__version__ for the PR')
+    parser.add_argument('--in_v', type=str, help='mplfinance.__version__ for the `master` branch')
     
     args = parser.parse_args()
     test_tag = 'VersionCheck:{}'
-    outcome = _version.compare_versions(v_str1=args.pr, v_str2=args.in)
+    outcome = _version.compare_versions(
+        v_str1=args.pr_v, 
+        v_str2=args.in_v
+    )
     print(test_tag.format('pr{}master'.format(outcome)))
