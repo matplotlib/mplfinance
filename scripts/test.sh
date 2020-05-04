ee9292e280a81647fa7433f4b74ec8bc7793a6b0
Aaron Soellinger <free-soellingeraj@Lydias-MacBook-Pro.local>
2020-05-04 15:25:00 +0200

    added command to exit travis build on error

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
 # Run this script test.sh from project root directory containing setup.py
+
+set -ev
+
 pr_branch=$1
-if ["${pr_branch}" == "false"]:
+if [ "${pr_branch}" == "false" ]
 then
     exit 0
 fi
 
+echo "PR: branch passed in: $pr_branch"
 pip3 install .
 git checkout ${pr_branch}
 pr_version=$(python3 -c "import mplfinance; print(mplfinance.__version__)")
@@ -14,8 +17,7 @@ git checkout master
 in_version=$(python3 -c "import mplfinance; print(mplfinance.__version__)")
 echo "PR: ${pr_version}; Incumbent: ${in_version}"
 result=$(python3 -m mplfinance.check_version --pr ${pr_version} --in ${in_version})
-tox
-if ["${result}" != "VersionCheck:pr>master"]
+if [ "${result}" != "VersionCheck:pr>master" ]
 then
     # version in PR doesn't pass the test
     echo "${result}"
