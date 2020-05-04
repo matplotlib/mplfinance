3fe2728bbaa465b23fa3cfbbb730447a78b43be5
Aaron Soellinger <free-soellingeraj@Lydias-MacBook-Pro.local>
2020-05-04 15:17:37 +0200

    fixed bug on if statement syntax

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,10 @@
 # Run this script test.sh from project root directory containing setup.py
 pr_branch=$1
 if ["${pr_branch}" == "false"]:
+then
     exit 0
+fi
+
 pip3 install .
 git checkout ${pr_branch}
 pr_version=$(python3 -c "import mplfinance; print(mplfinance.__version__)")
@@ -13,6 +16,7 @@ echo "PR: ${pr_version}; Incumbent: ${in_version}"
 result=$(python3 -m mplfinance.check_version --pr ${pr_version} --in ${in_version})
 tox
 if ["${result}" != "VersionCheck:pr>master"]
+then
     # version in PR doesn't pass the test
     echo "${result}"
     exit 1
