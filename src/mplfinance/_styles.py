aae559a57951dc7f9db29e46b42e0de3633cc50b
Kenan Arik <arik.kenan@gmail.com>
2021-01-06 01:01:30 -0800

    removed up_hollow and down_hollow from valid marketcolor keywords

--- src/mplfinance/_styles.py
+++ src/mplfinance/_styles.py
@@ -159,12 +159,6 @@ def _valid_make_marketcolors_kwargs():
 
         'down'       : { 'Default'     : None,
                          'Validator'   : lambda value: mcolors.is_color_like(value) },
-        
-        'up_hollow'     : { 'Default'     : None,
-                         'Validator'   : lambda value: mcolors.is_color_like(value) },
-        
-        'down_hollow'     : { 'Default'     : None,
-                         'Validator'   : lambda value: mcolors.is_color_like(value) },
 
         'alpha'       : { 'Default'     : None,
                          'Validator'   : lambda value: ( isinstance(value,float) and
@@ -225,7 +219,6 @@ def make_marketcolors(**kwargs):
 
     up   = config['up']
     down = config['down']
-    hollow = config['down']
     if up is not None and down is not None:
         marketcolors.update(candle=dict(up=up,down=down))
     elif up is not None:
@@ -236,10 +229,7 @@ def make_marketcolors(**kwargs):
         candle = marketcolors['candle']
         candle.update(down=down)
         marketcolors.update(down=down)
-    elif hollow is not None:
-        candle = marketcolors['candle']
-        candle.update(hollow=hollow)
-        marketcolors.update(hollow=hollow)
+
 
     def _check_and_set_mktcolor(candle,**kwarg):
         if len(kwarg) != 1:
