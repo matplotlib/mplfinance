cd85a86641d55d18f680931f294efe2bfaec0adb
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2021-02-25 21:47:00 -0500

    try original failure, before trying `pip install --upgrade pip`

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -13,16 +13,16 @@ fi
 echo "PR: branch passed in: $pr_branch"
 git fetch origin +refs/pull/${pr_branch}/merge
 git checkout FETCH_HEAD
-pip3 install --use-feature=2020-resolver --upgrade-strategy eager .
+pip3 install .
 pr_version=$(python3 -c "import mplfinance; print(mplfinance.__version__)")
 git checkout master
-pip3 install --use-feature=2020-resolver --upgrade-strategy eager .
+pip3 install .
 in_version=$(python3 -c "import mplfinance; print(mplfinance.__version__)")
 echo "PR: ${pr_version}; Incumbent: ${in_version}"
 
 # test the versions
 git checkout FETCH_HEAD
-pip3 install --use-feature=2020-resolver --upgrade-strategy eager .
+pip3 install .
 result=$(python3 scripts/check_version.py --pr_v ${pr_version} --in_v ${in_version})
 if [ "${result}" != "VersionCheck:pr>master" ]
 then
