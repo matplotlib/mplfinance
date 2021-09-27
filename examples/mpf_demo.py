7c7ed5abc07441fe6b4a5033a07491d9ed126980
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2021-09-27 13:41:26 -0400

    tweak demo scripts

--- examples/mpf_demo.py
+++ examples/mpf_demo.py
@@ -5,11 +5,17 @@ infile = 'data/yahoofinance-SPY-20200901-20210113.csv'
 
 df = pd.read_csv(infile, index_col=0, parse_dates=True).iloc[0:60]
 
-# mpf.plot(df,type='candle',volume=True,mav=(10,20),figscale=1.5)
+# mpf.plot(df,figscale=1.5,type='candle',mav=(10,20))
 
-import rsi
-df['rsi'] = rsi.relative_strength(df['Close'],n=7)
 
-apd = mpf.make_addplot(df['rsi'],panel=1,color='lime',ylim=(10,90),secondary_y=True)
 
-mpf.plot(df,type='candle',volume=True,mav=(10,20),figscale=1.5,addplot=apd,style='checkers')
+
+#mpf.plot(df,type='candle',figscale=1.5)
+
+#df = pd.read_csv(infile, index_col=0, parse_dates=True).iloc[0:180]
+#mpf.plot(df,type='renko',figscale=1.5)
+#mpf.plot(df,type='pnf',figscale=1.5)
+
+#mpf.plot(df,type='candle',figscale=1.5,mav=10) 
+
+mpf.plot(df,type='candle',volume=True,mav=(10,20),figscale=1.5)
