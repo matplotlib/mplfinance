2ea49e4fdc6dc017686f1b29190cefa7398a512e
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2022-10-31 20:41:27 -0400

    check for whitespace when column not found; Issue #496

--- src/mplfinance/_arg_validators.py
+++ src/mplfinance/_arg_validators.py
@@ -30,6 +30,53 @@ def _check_and_prepare_data(data, config):
     if not isinstance(data.index,pd.core.indexes.datetimes.DatetimeIndex):
         raise TypeError('Expect data.index as DatetimeIndex')
 
+    # We will not be fully case-insensitive (since Pandas columns as NOT case-insensitive)
+    # but because so many people have requested it, for the default column names we will
+    # try both Capitalized and lower case:
+    columns = config['columns']
+    if columns is None:
+        columns =  ('Open', 'High', 'Low', 'Close', 'Volume')
+        if all([c.lower() in data for c in columns[0:4]]):
+            columns =  ('open', 'high', 'low', 'close', 'volume')
+        
+    o, h, l, c, v = columns
+    cols = [o, h, l, c]
+
+    if config['volume'] != False:
+        expect_cols = columns
+    else:
+        expect_cols = cols
+    
+    for c in expect_cols:
+        if c not in data.columns:
+            for dc in data.columns:
+                if dc.strip() != dc:
+                    warnings.warn('\n ================================================================= '+
+                                  '\n   Input DataFrame column name "'+dc+'" '+
+                                  '\n   contains leading and/or trailing whitespace.',category=UserWarning)
+            raise ValueError('Column "'+c+'" NOT FOUND in Input DataFrame!'+
+                             '\n            CHECK that your column names are correct AND/OR'+
+                             '\n            CHECK for leading or trailing blanks in your column names.')
+
+    opens   = data[o].values
+    highs   = data[h].values
+    lows    = data[l].values
+    closes  = data[c].values
+    if v in data.columns:
+        volumes = data[v].values
+        cols.append(v)
+    else:
+        volumes = None
+
+    for col in cols:
+        if not all( isinstance(v,(float,int)) for v in data[col] ):
+            raise ValueError('Data for column "'+str(col)+'" must be ALL float or int.')
+
+    if config['tz_localize']:
+        dates   = mdates.date2num(data.index.tz_localize(None).to_pydatetime())
+    else:  # Just in case someone was depending on this bug (Issue 236)
+        dates   = mdates.date2num(data.index.to_pydatetime())
+
     if (len(data.index) > config['warn_too_much_data'] and
         (config['type']=='candle' or config['type']=='ohlc' or config['type']=='hollow_and_filled')
        ):
@@ -44,36 +91,6 @@ def _check_and_prepare_data(data, config):
                         '\n   LARGER than the number of data points you want to plot.'+
                       '\n\n ================================================================ ',
                   category=UserWarning)
-
-    # We will not be fully case-insensitive (since Pandas columns as NOT case-insensitive)
-    # but because so many people have requested it, for the default column names we will
-    # try both Capitalized and lower case:
-    columns = config['columns']
-    if columns is None:
-        columns =  ('Open', 'High', 'Low', 'Close', 'Volume')
-        if all([c.lower() in data for c in columns[0:4]]):
-            columns =  ('open', 'high', 'low', 'close', 'volume')
-        
-    o, h, l, c, v = columns
-    cols = [o, h, l, c]
-
-    if config['tz_localize']:
-        dates   = mdates.date2num(data.index.tz_localize(None).to_pydatetime())
-    else:  # Just in case someone was depending on this bug (Issue 236)
-        dates   = mdates.date2num(data.index.to_pydatetime())
-    opens   = data[o].values
-    highs   = data[h].values
-    lows    = data[l].values
-    closes  = data[c].values
-    if v in data.columns:
-        volumes = data[v].values
-        cols.append(v)
-    else:
-        volumes = None
-
-    for col in cols:
-        if not all( isinstance(v,(float,int)) for v in data[col] ):
-            raise ValueError('Data for column "'+str(col)+'" must be ALL float or int.')
 
     return dates, opens, highs, lows, closes, volumes
 
